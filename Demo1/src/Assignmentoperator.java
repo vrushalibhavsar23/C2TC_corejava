1aa7dd352157801a10143f8edf0ec6e15453be47
vrush <vrush@DESKTOP-L89752I>
2022-08-16 14:28:23 +0530

    first commit

---
+++ Demo1/src/Assignmentoperator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Assignmentoperator {
+	public static void main(String[] args) {
+	int a = 30;
+	int b=20;
+    
+	System.out.println(	a+=b);
+	System.out.println(a-=b);
+	System.out.println(a/=10);
+	System.out.println(a%=5);
+	System.out.println(b*=2);
+	
+    
+}
+}
